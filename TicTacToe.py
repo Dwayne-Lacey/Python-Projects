59e707425f0ab57f0b4d6d4c8a28f4b3f2ef0c38
Dwyane Lacey <dwaynelacey.acm@gmail.com>
2022-01-19 16:36:50 -0500

    Added the title of the game

---
+++ TicTacToe.py
@@ -0,0 +1,20 @@
+from random import randint
+
+class Game_Grid:
+    def __init__(self):
+        pass
+        
+
+
+
+
+#Code to add game title at top of terminal
+print("------------------------------------------ ")
+print("### #  ###  ###  ##   ###  ###  ##  ####")
+print(" #  # #      #  #  # #      #  #  # #")
+print(" #  # #      #  #### #      #  #  # ###")
+print(" #  # #      #  #  # #      #  #  # #")
+print(" #  #  ###   #  #  #  ###   #   ##  ####")
+print("------------------------------------------ ")
+
+
