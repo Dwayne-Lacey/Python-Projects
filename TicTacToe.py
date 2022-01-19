012b637f89a19d4b41a4232552ee8eb6d8cf72e3
Dwyane Lacey <dwaynelacey.acm@gmail.com>
2022-01-19 17:51:54 -0500

    Added initial class variables and began build_grid method

--- TicTacToe.py
+++ TicTacToe.py
@@ -1,11 +1,20 @@
 from random import randint
 
+#This class creates an instance of the Tic-Tac-Toe game 
 class Game_Grid:
-    def __init__(self):
+    def __init__(self, players, player_shape, grid_size):
+        self.turn = 0
+        self.players = players
+        self.game_complete = ""
+        self.game_grid = {}
+        self.player_shape = player_shape
+        self.grid_size = grid_size
+
+    def build_grid(self):
         pass
         
 
-#Initializa variables here
+#Initialize variables here
 start_game = ""
 
 #Code to add game title at top of terminal
@@ -17,8 +26,6 @@ print(" #  # #      #  #  # #      #  #  # #")
 print(" #  #  ###   #  #  #  ###   #   ##  ####")
 print("------------------------------------------ ")
 
+
 while start_game != "y" and start_game != "n":
     start_game = input("Welcome to Tic-Tac-Toe! Would you like to start a new game? y/n ").lower()
-    print(start_game)
-    print(start_game != "y")
-    print(start_game != "n")
