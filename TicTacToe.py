d76c39daa695b2a17f38732024bcf1169a32c54f
Dwyane Lacey <dwaynelacey.acm@gmail.com>
2022-01-19 17:49:45 -0500

    Added start game option and created initilizers for variables

--- TicTacToe.py
+++ TicTacToe.py
@@ -5,8 +5,8 @@ class Game_Grid:
         pass
         
 
-
-
+#Initializa variables here
+start_game = ""
 
 #Code to add game title at top of terminal
 print("------------------------------------------ ")
@@ -17,4 +17,8 @@ print(" #  # #      #  #  # #      #  #  # #")
 print(" #  #  ###   #  #  #  ###   #   ##  ####")
 print("------------------------------------------ ")
 
-
+while start_game != "y" and start_game != "n":
+    start_game = input("Welcome to Tic-Tac-Toe! Would you like to start a new game? y/n ").lower()
+    print(start_game)
+    print(start_game != "y")
+    print(start_game != "n")
